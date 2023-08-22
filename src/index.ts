dcd3e58f64545aeebd09cac7d0b21a684bc76e94
Ershad Qaderi <ershad.qaderi@gmail.com>
2023-08-23 01:00:34 +0200

    Add cleanUrl

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,10 @@ type ShortenParams = {
   short?: string;
 };
 
-export async function shortenUrl(url: string, customShort?: string): Promise<string> {
+async function shortenWith1pt(
+  url: string,
+  customShort?: string
+): Promise<string> {
   const shorteningEndpoint =
     "https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL";
 
@@ -40,4 +43,51 @@ export async function shortenUrl(url: string, customShort?: string): Promise<str
     console.error("An error occurred while shortening the URL:", error);
     throw error;
   }
+}
+
+async function shortenWithCleanUri(url: string): Promise<string> {
+  const cleanUriEndpoint = "https://cleanuri.com/api/v1/shorten";
+
+  try {
+    const response: AxiosResponse<{ result_url: string }> = await axios.post<{
+      result_url: string;
+    }>(cleanUriEndpoint, `url=${encodeURIComponent(url)}`);
+
+    if (response.data && response.data.result_url) {
+      return response.data.result_url;
+    }
+
+    throw new Error("Failed to shorten URL with CleanUri");
+  } catch (error) {
+    console.error(
+      "An error occurred while shortening the URL with CleanUri:",
+      error
+    );
+    throw error;
+  }
+}
+
+type ShorteningService = "1pt" | "cleanuri"; // Extend this as needed
+type ShorteningFunction = (
+  url: string,
+  customShort?: string
+) => Promise<string>;
+
+const shortenWithService: Record<ShorteningService, ShorteningFunction> = {
+  "1pt": shortenWith1pt,
+  cleanuri: shortenWithCleanUri,
+};
+
+export async function shortenUrl(
+  url: string,
+  service: ShorteningService = '1pt',
+  customShort?: string
+): Promise<string> {
+  const shortenFunction = shortenWithService[service];
+
+  if (shortenFunction) {
+    return shortenFunction(url, customShort);
+  }
+
+  throw new Error("Unsupported shortening service");
 }
