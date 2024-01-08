9946be899198510f3fa917fa615375c59f29d796
Ershad Qaderi <ershad.qaderi@gmail.com>
2024-01-08 17:05:02 +0100

    Use cleanuri as default formatter

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 import { shortenWith1pt } from './shorteningServices/one-pt'
 import { shortenWithCleanUri } from './shorteningServices/cleanuri'
 import { shortenWithIsgd, IsgdParamsType } from './shorteningServices/isgd'
-import {shortenWithShrtcode } from './shorteningServices/shrtcode'
+import { shortenWithShrtcode } from './shorteningServices/shrtcode'
 
-export type ShorteningService = "1pt" | "cleanuri" | "isgd" | "shrtcode";
+export type ShorteningService = '1pt' | 'cleanuri' | 'isgd' | 'shrtcode'
 type ShorteningFunction = (
   url: string,
   customShort?: string,
-  format?: IsgdParamsType["format"],
+  format?: IsgdParamsType['format'],
   logstats?: boolean
-) => Promise<string>;
+) => Promise<string>
 
 const shortenWithService: Record<ShorteningService, ShorteningFunction> = {
-  "1pt": shortenWith1pt,
+  '1pt': shortenWith1pt,
   cleanuri: shortenWithCleanUri,
   isgd: shortenWithIsgd,
   shrtcode: shortenWithShrtcode,
-};
+}
 
 export async function shortenUrl(
   url: string,
-  service: ShorteningService = "isgd",
+  service: ShorteningService = 'cleanuri',
   customShort?: string
 ): Promise<string> {
-  const shortenFunction = shortenWithService[service];
+  const shortenFunction = shortenWithService[service]
 
   if (shortenFunction) {
-    return shortenFunction(url, customShort);
+    return shortenFunction(url, customShort)
   }
 
-  throw new Error("Unsupported shortening service");
+  throw new Error('Unsupported shortening service')
 }
